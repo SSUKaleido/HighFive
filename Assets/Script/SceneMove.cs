473d8d0fc3e792aa68a28cadcf74a9a0cff37b03
yeonsoo7 <rladustn7373@gmail.com>
2023-03-03 17:54:09 +0900

    메인메뉴

---
+++ Assets/Script/SceneMove.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMove : MonoBehaviour
+{
+    public void MoveToB1(){
+        SceneManager.LoadScene("Beet_1");
+    }
+    public void MoveToB2(){
+        SceneManager.LoadScene("Beet_2");
+    }
+    public void MoveToB3(){
+        SceneManager.LoadScene("Beet_3");
+    }
+    public void MoveToR1(){    //리듬게임1로 이동
+        SceneManager.LoadScene("Rhythm1");
+    }
+    public void MoveToR2(){    //리듬게임2로 이동
+        SceneManager.LoadScene("Rhythm2");
+    }
+    public void MoveToR3(){    //리듬게임3으로 이동
+        SceneManager.LoadScene("Rhythm3");
+    }
+
+
+
+
+
+    public void GameExit(){    //게임나가기
+        Application.Quit();
+    }
+}
