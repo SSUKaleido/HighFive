4dc657b3962c404747f14676af818c5049d9c89c
naboyeong <nabo2000@naver.com>
2023-03-05 12:25:50 +0900

    엔딩 점수 추가

---
+++ Assets/Script/ScoreManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ScoreManager
+{
+    public static int BeetS = 0;
+    public static int MozartS = 0;
+
+    
+}
